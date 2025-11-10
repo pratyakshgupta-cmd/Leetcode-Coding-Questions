0a91886108c75fff24019dcdbe239268727e6015
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2025-11-10 09:43:50 +0530

    Time: 0 ms (100%), Space: 42.1 MB (5.45%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] ar = new int[n][n];
+        int[][] ar = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0||j==0){
@@ -11,6 +11,6 @@ class Solution {
                 }
             }
         }
-        return ar[m-2][n-1];
+        return ar[m-1][n-1];
     }
 }
