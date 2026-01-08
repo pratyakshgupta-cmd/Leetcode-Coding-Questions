f58347cec98b07fe14b407663c3318614380ff16
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-01-08 14:44:07 +0530

    Time: 2 ms (99.12%), Space: 47.2 MB (23.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,19 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int ans[]=new int[2];
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    ans[0]=i;
-                    ans[1]=j;
-                    //break : it is not imp to write for optimization we write this 
-                    break;
-                }
-            }
-        }
-        return ans;
-    }
-}
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int ans[]=new int[2];
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     ans[0]=i;
+//                     ans[1]=j;
+//                     //break : it is not imp to write for optimization we write this 
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
 
 
 // class Solution {
@@ -31,3 +31,30 @@ class Solution {
 //         return ans;
 //     }
 // }
+
+
+// hasmap is use to store 2 things at the same time. in this we will put value and its index
+class Solution{
+    public int[] twoSum(int[] nums,int target){
+        // we will put things in map one by one then campare things for making 9 if not then store 
+
+        //[2,11,15,7]
+        //map=[2:0,11:1,15:2]
+         //parent : map child: hashmap
+
+
+        HashMap<Integer,Integer> m=new HashMap<>();
+        int ans[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            int othervalue= target-nums[i];
+            if(m.containsKey(othervalue)){
+                ans[0]=m.get(othervalue);
+                ans[1]=i;
+                break;
+            }
+            m.put(nums[i],i);
+        }
+        return ans;
+
+    }
+}
